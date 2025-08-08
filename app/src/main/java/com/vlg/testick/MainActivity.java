3d7e714855aac7784b4178af625af41690e2b2a3
vadim <gabderahmanov99@gmail.com>
2025-08-08 19:18:27 +0300

    version of language creating tests (lct)

--- app/src/main/java/com/vlg/testick/MainActivity.java
+++ app/src/main/java/com/vlg/testick/MainActivity.java
@@ -9,6 +9,42 @@ import com.vlg.testick.ui.QuestionFragment;
 
 public class MainActivity extends AppCompatActivity implements Navigation {
 
+/**
+    1 [title]
+    2 [titleh1]
+    style: [[style], default]
+    question [name]
+    [{[type] [name] [text] [value] [isRight] [newLine]},
+    {[type] [name] [text] [value] [isRight] [newLine]},
+    ...]
+    ...
+
+    title token - title, t, nameQuiz, квизСайт, названиеСайт
+    titleH1 token - h1, titleH1, th1, quiz, квиз, название, заголовок
+    style token - s. style, с, стиль
+    question token - ?, question, q, вопрос, в
+    radio token - radio, r, радио, р
+    checkbox token - c, chex, checkbox, ч, чекбокс, чек
+    text token - t, text, текст, т
+    end line token - e
+    right answer token - +, r, к
+
+    title title
+    titleQuiz h1
+    style default
+
+    ? who r q1
+    val1 "dr. who"
+    val2 "i am" +
+
+    ? second c
+    v1 v1 "fffff"
+    v2 v2 "ffff" +
+    v3 v3 "ffffss"
+
+    ? third
+    text q1 "who is fuck" ok n
+ **/
     private App app;
 
     public App getApp() {
